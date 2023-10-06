ec5ac3452ffc23be08dd072e6203d8ce3b5b0a87
redaezziani <2003100500167@eofppt-edu.ma>
2023-10-06 10:56:55 +0100

    add

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,34 @@
 import './css/App.css'
-
+import {motion} from 'framer-motion'
 function App() {
 
   return (
-    <div className="w-full flex h-screen justify-center items-center">
-      <button
+    <div className="w-full flex-col gap-2  flex h-screen justify-center items-center">
+      <motion.button
+        initial={{ opacity: 0, scale: 0 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 1, type: "spring", stiffness: 100 }}
         type="button"
-        className=" btn-twiter   btn  "
+        className="btn btn-apple "
       >
+        Check out with Apple Pay
         <svg
-          className="w-4 h-4 mr-2"
+          className="w-5 h-5 ml-2 -mr-1"
           aria-hidden="true"
+          focusable="false"
+          data-prefix="fab"
+          data-icon="apple"
+          role="img"
           xmlns="http://www.w3.org/2000/svg"
-          fill="currentColor"
-          viewBox="0 0 20 17"
+          viewBox="0 0 384 512"
         >
           <path
-            fillRule="evenodd"
-            d="M20 1.892a8.178 8.178 0 0 1-2.355.635 4.074 4.074 0 0 0 1.8-2.235 8.344 8.344 0 0 1-2.605.98A4.13 4.13 0 0 0 13.85 0a4.068 4.068 0 0 0-4.1 4.038 4 4 0 0 0 .105.919A11.705 11.705 0 0 1 1.4.734a4.006 4.006 0 0 0 1.268 5.392 4.165 4.165 0 0 1-1.859-.5v.05A4.057 4.057 0 0 0 4.1 9.635a4.19 4.19 0 0 1-1.856.07 4.108 4.108 0 0 0 3.831 2.807A8.36 8.36 0 0 1 0 14.184 11.732 11.732 0 0 0 6.291 16 11.502 11.502 0 0 0 17.964 4.5c0-.177 0-.35-.012-.523A8.143 8.143 0 0 0 20 1.892Z"
-            clipRule="evenodd"
-          />
+            fill="currentColor"
+            d="M318.7 268.7c-.2-36.7 16.4-64.4 50-84.8-18.8-26.9-47.2-41.7-84.7-44.6-35.5-2.8-74.3 20.7-88.5 20.7-15 0-49.4-19.7-76.4-19.7C63.3 141.2 4 184.8 4 273.5q0 39.3 14.4 81.2c12.8 36.7 59 126.7 107.2 125.2 25.2-.6 43-17.9 75.8-17.9 31.8 0 48.3 17.9 76.4 17.9 48.6-.7 90.4-82.5 102.6-119.3-65.2-30.7-61.7-90-61.7-91.9zm-56.6-164.2c27.3-32.4 24.8-61.9 24-72.5-24.1 1.4-52 16.4-67.9 34.9-17.5 19.8-27.8 44.3-25.6 71.9 26.1 2 49.9-11.4 69.5-34.3z"
+          ></path>
         </svg>
-        Sign in with Twitter
-      </button>
+
+      </motion.button>
     </div>
   );
 }
